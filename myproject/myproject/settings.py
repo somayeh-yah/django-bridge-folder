b2791edb2ec7c23cc2bf28631593297f6a55ed5d
somayeh-yah <somayeh.yahyaei@outlook.com>
2025-11-14 19:50:44 +0100

    correct Django template blocks and clean up formatting issues

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1wyvmhhcy!-%wb5kabnfjypp=_p1+k*+v=0!5w*3&47r+_iemy"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
